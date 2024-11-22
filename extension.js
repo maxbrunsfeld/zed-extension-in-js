d4f567b1089063eb334a99a23b70681004b07302
Max Brunsfeld <maxbrunsfeld@gmail.com>
2024-11-22 15:48:53 -0800

    Upgrade componentize-js

--- extension.js
+++ extension.js
@@ -4,3 +4,35 @@ export function initExtension () {
 export function languageServerCommand() {
   return { command: "", args: [], env: {} }
 }
+
+export function languageServerInitializationOptions() {
+  return {}
+}
+
+export function languageServerWorkspaceConfiguration() {
+  return {}
+}
+
+export function labelsForCompletions() {
+
+}
+
+export function labelsForSymbols() {
+
+}
+
+export function completeSlashCommandArgument() {
+
+}
+
+export function runSlashCommand() {
+
+}
+
+export function suggestDocsPackages() {
+
+}
+
+export function indexDocs() {
+
+}
